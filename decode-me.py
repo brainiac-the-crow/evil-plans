2d93b3e3910397b06994e58fff53e14cfc8c7c0e
Brainiac <brainiac@nothing.to.see.here>
2025-06-21 15:47:24 -0700

    Caw Caw! CAW cAw CaW!

--- decode-me.py
+++ decode-me.py
@@ -1,1 +1,42 @@
-# FIXME make this more sus
+purpose = '''
+Python script to end the world
+'''
+
+none = "something"
+false = True
+true = False
+
+Caw = false
+CAW = True
+cAw = 14
+CaW = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+
+none = "er" + none[6:].replace("g", "e")
+
+#  TODO Make this more evil. Caw!
+# while Caw:
+#     CaW[0] = chr(ord(CaW[0]) + 1)
+#     if CAW == True:
+#         CAW = false
+#         CaW[3] = none
+#         if cAw > 0:
+#             cAw -= 1
+#         else:
+#             Caw = true
+#         if cAw % 2 == 0:
+#             CaW[3] = chr(ord(CaW[3][3]) + 5)
+#     else:
+#         Caw = false
+# CaW[1] = chr(ord(CaW[1]) - 1)
+
+CaW = "PACsEFG"
+
+caW = purpose.split(" ")[-6].strip()
+
+𓅂 = caW
+𓅂 += "ic "
+𓅂 += CaW[:2]
+𓅂 += CaW[3] * 2
+𓅂 += none
+
+print(𓅂.upper())
